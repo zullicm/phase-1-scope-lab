eb88fc535f2911bf3f483886988922e5d150f8ff
Christopher Marzulli <chriszulli15@gmail.com>
2022-04-27 11:29:27 -0400

    commit tree 099e121d75595b7f0f8bda5a096ce85ee71b0513

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    if (customerName === 'bob'){
+        return customerName = 'BOB'
+    }
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'chris'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
